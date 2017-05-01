5cd045aee315918b64032c347d10517028eb2eed
srbiz <sidneibiz@gmail.com>
2017-05-01 00:47:28 -0300

    add credits and add debts

--- frontend/app/billing-cycle/billing-cycle.controller.js
+++ frontend/app/billing-cycle/billing-cycle.controller.js
@@ -12,7 +12,7 @@
 
     vm.refresh = () => {
       $http.get(url).then(response => {
-        vm.billingCycle = {};
+        vm.billingCycle = { credits: [{}], debts: [{}] };
         vm.billingCycles = response.data;
         tabs.show(vm, { tabList: true, tabCreate: true });
       });
@@ -49,6 +49,30 @@
     vm.showTabDelete = (billingCycle) => {
       vm.billingCycle = billingCycle;
       tabs.show(vm, { tabDelete: true });
+    };
+
+    vm.addCredit = (index) => {
+      vm.billingCycle.credits.splice(index + 1, 0, {});
+    };
+
+    vm.cloneCredit = (index, { name, value }) => {
+      vm.billingCycle.credits.splice(index + 1, 0, { name, value });
+    };
+
+    vm.deleteCredit = (index) => {
+      if (vm.billingCycle.credits.length > 1) vm.billingCycle.credits.splice(index, 1);
+    };
+
+    vm.addDebt = (index) => {
+      vm.billingCycle.debts.splice(index + 1, 0, {});
+    };
+
+    vm.cloneDebt = (index, { name, value, status }) => {
+      vm.billingCycle.debts.splice(index + 1, 0, { name, value, status });
+    };
+
+    vm.deleteDebt = (index) => {
+      if (vm.billingCycle.debts.length > 1) vm.billingCycle.debts.splice(index, 1);
     };
 
     vm.refresh();
